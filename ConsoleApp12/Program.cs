adff45d3cd71580f1a1146ad00c9b195430a598d
OĞUZ ERDOĞAN <oguzerdogan@ogr.trakya.edu.tr>
2024-07-06 21:04:55 +0300

    ExelToJPG

--- ConsoleApp12/Program.cs
+++ ConsoleApp12/Program.cs
@@ -1,16 +1,9 @@
 ﻿using Microsoft.Office.Interop.Excel;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Drawing.Imaging;
-using System.IO;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
-using ios = System.Runtime.InteropServices;
 
 class Program
 {
